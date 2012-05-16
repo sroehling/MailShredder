1775e78b37685d06a3988f5b1edbf4b90387b609
Steve Roehling <sroehling@resultra.com>
2012-05-16 13:36:32 -0700

    Remove the workspace file from the project folder.

---
+++ MailCleaner/EmailInfoTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  EmailInfoViewController.h
+//  MailCleaner
+//
+//  Created by Steve Roehling on 5/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EmailInfoViewController : UIViewController
+
+@end
