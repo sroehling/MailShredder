ce49482b862cb1badd78780bcf5f730c87415add
Steve Roehling <sroehling@resultra.com>
2012-09-06 08:17:21 -0700

    Fix a core data merge conflict which would intermittently occur when counting the messages matching different filters.
    The fix included merging from the main NSManagedObjectContext into the one in the counting thread,
    queueing up core data change notifications and merging any changes before saving the counts.

--- MailCleaner/MessageFilterCountOperation.h
+++ MailCleaner/MessageFilterCountOperation.h
@@ -16,10 +16,17 @@
 	@private
 		EmailAccount *emailAccount;
 		DataModelController *mainThreadDmc;
+		DataModelController *msgCountsDmc;
+		NSMutableArray *cachedChangeNotifications;
+		NSLock  * changeNotificationsLock;
 }
 
 @property(nonatomic,retain) EmailAccount *emailAccount;
 @property(nonatomic,retain) DataModelController *mainThreadDmc;
+@property(nonatomic,retain) DataModelController *msgCountsDmc;
+
+@property(nonatomic,retain) NSMutableArray *cachedChangeNotifications;
+@property(nonatomic,retain) NSLock  * changeNotificationsLock;
 
 -(id)initWithMainThreadDmc:(DataModelController*)mainThreadDmc
 	andEmailAccount:(EmailAccount*)theEmailAccount;
--- MailCleaner/MessageFilterCountOperation.m
+++ MailCleaner/MessageFilterCountOperation.m
@@ -18,6 +18,9 @@
 
 @synthesize emailAccount;
 @synthesize mainThreadDmc;
+@synthesize msgCountsDmc;
+@synthesize cachedChangeNotifications;
+@synthesize changeNotificationsLock;
 
 -(id)initWithMainThreadDmc:(DataModelController*)theMainThreadDmc
 	andEmailAccount:(EmailAccount*)theEmailAccount
@@ -30,6 +33,9 @@
 		
 		assert(theMainThreadDmc != nil);
 		self.mainThreadDmc = theMainThreadDmc;
+		
+		self.cachedChangeNotifications = [[[NSMutableArray alloc] init] autorelease];
+		self.changeNotificationsLock = [[[NSLock alloc] init] autorelease];
 	}
 	return self;
 }
@@ -38,30 +44,51 @@
 {
 	[emailAccount release];
 	[mainThreadDmc release];
+	[msgCountsDmc release];
+	[cachedChangeNotifications release];
+	[changeNotificationsLock release];
 	[super dealloc];
 }
 
 -(void)msgCountsThreadDidSaveNotificationHandler:(NSNotification*)notification
 {
+	[self.changeNotificationsLock lock];
 	[self.mainThreadDmc.managedObjectContext mergeChangesFromContextDidSaveNotification:notification];
+	[self.changeNotificationsLock unlock];
+
 }
+
+-(void)mainThreadDidSaveNotificationHandler:(NSNotification*)notification
+{
+	[self.cachedChangeNotifications addObject:notification];
+}
+
 
 -(void)main
 {
 	NSLog(@"Starting operation to update message filter counts");
+
+	self.msgCountsDmc = [[[DataModelController alloc] 
+				initWithPersistentStoreCoord:self.mainThreadDmc.managedObjectContext.persistentStoreCoordinator] autorelease];
+	[self.msgCountsDmc.managedObjectContext setMergePolicy:NSMergeByPropertyStoreTrumpMergePolicy];
+
 	
 	@autoreleasepool {
-		DataModelController *msgCountsDmc = [[[DataModelController alloc] 
-				initWithPersistentStoreCoord:self.mainThreadDmc.managedObjectContext.persistentStoreCoordinator] autorelease];
 				
 		[[NSNotificationCenter defaultCenter] addObserver:self 
 			selector:@selector(msgCountsThreadDidSaveNotificationHandler:)
 			name:NSManagedObjectContextDidSaveNotification 
 			object:msgCountsDmc.managedObjectContext];
+	
+		[[NSNotificationCenter defaultCenter] addObserver:self 
+			selector:@selector(mainThreadDidSaveNotificationHandler:)
+			name:NSManagedObjectContextDidSaveNotification 
+			object:mainThreadDmc.managedObjectContext];
 			
+							
 		@try {
 			EmailAccount *acctInMsgCountingThread = (EmailAccount*)[CoreDataHelper 
-				objectInOtherContext:msgCountsDmc.managedObjectContext forOriginalObj:self.emailAccount];
+				objectInOtherContext:self.msgCountsDmc.managedObjectContext forOriginalObj:self.emailAccount];
 				
 			for(MessageFilter *savedFilter in acctInMsgCountingThread.savedMsgListFilters)
 			{
@@ -72,21 +99,39 @@
 				NSArray *matchingMsgs = [CoreDataHelper executeFetchOrThrow:savedFilterFetch 
 						inManagedObectContext:msgCountsDmc.managedObjectContext];
 				NSLog(@"Matching message count for filter: %@ = %d",savedFilter.filterName,[matchingMsgs count]);
-				savedFilter.matchingMsgs = [NSNumber numberWithInt:[matchingMsgs count]];
-	 
+				savedFilter.matchingMsgs = [NSNumber numberWithInt:[matchingMsgs count]];	 
 			}
 			
-			[msgCountsDmc saveContext];
+			[self.changeNotificationsLock lock];
+			for(NSNotification *cachedNotification in self.cachedChangeNotifications)
+			{
+				NSLog(@"Merging cached changes from main thread MOC to msg filter counting thread MOC");
+				[self.msgCountsDmc.managedObjectContext mergeChangesFromContextDidSaveNotification:cachedNotification];
+			}
+			[self.cachedChangeNotifications removeAllObjects];
+			[self.changeNotificationsLock unlock];
+			
+			[self.msgCountsDmc saveContext];
+
+			NSLog(@"Done with operation to updat e message filter counts");
+
+
 		}
 		@catch (NSException *exception)
 		{
-			;
+			// If the message counting fails for some reason, it is not critical. The counts
+			// are only used for display in the UI.
 		}
 		@finally 
 		{
 			[[NSNotificationCenter defaultCenter] removeObserver:self 
 					name:NSManagedObjectContextDidSaveNotification 
 					object:msgCountsDmc.managedObjectContext];
+
+			[[NSNotificationCenter defaultCenter] removeObserver:self 
+					name:NSManagedObjectContextDidSaveNotification 
+					object:mainThreadDmc.managedObjectContext];
+
 		}
 
    
