2eeeea407c3b55beed8ea7c75d3c66f432cf914b
Steve Roehling <sroehling@resultra.com>
2014-04-28 12:24:15 -0700

    Implement a delegate method to save the app’s core data context when the app is closed, or is switched out for a phone call, another app, etc. This is necessary to ensure any message filters, settings, etc. are always saved.

--- MailCleaner/AppDelegate.m
+++ MailCleaner/AppDelegate.m
@@ -296,6 +296,17 @@
 	 [self configStartingViewController];
 }
 
+- (void)applicationWillResignActive:(UIApplication *)application
+{
+    /*
+     Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+     Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+     */
+    [self.appDmc saveContext];
+    
+}
+
+
 
 ////////////////////////////////////////////////////////////////
 // The methods below support global, background operations for 
